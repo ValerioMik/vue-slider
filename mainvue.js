718e86f869afb80e2ad29985d1301f4c1fd1c7e5
ValerioMik <valerio.avallone@hotmail.com>
2021-11-11 15:38:25 +0100

    prima fase

---
+++ mainvue.js
@@ -0,0 +1,34 @@
+var carusel = new Vue({
+    el:".container",
+    data:{
+        activeslide:0,
+        slides:[
+            {
+                image:'img/01.jpg',
+                title:'Svezia',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image:'img/02.jpg',
+                title:'Svizzera',
+                text:'Lorem ipsum'
+            },
+            {
+                image:'img/03.jpg',
+                title:'Gran bretagna',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                image:'img/04.jpg',
+                title:'Germania',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            }, 
+            {
+                image:'img/05.jpg',
+                title:'Un altro posto',
+                text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            }
+        ],
+        
+    }
+});
